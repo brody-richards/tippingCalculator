eb5602728cab1292ec88eb2d2c2a2df8221e606f
brodyedwardrichards <brody.richards@edu.sait.ca>
2025-01-27 16:42:47 -0700

    final files

--- index.php
+++ index.php
@@ -9,30 +9,27 @@
 <body>
     <main>
         <section class="tippingCalculator">
-            <h1>Tipping Calculator</h1>
-            <p>Enter the following information to split the check.</p>
-
+                <h1>Tipping Calculator</h1>
+                <p>Enter the following information to split the check.</p>
             <div class="tippingForm">
-            <form action="result.php" name="tippingCalculator" method="post">
-                <div class="people">
-                    <label for="numberOfPeople">Number of Diners:</label>
-                    <input type="number" name="numberOfPeople" id="numberOfPeople" min="1">
-                </div>
-                <div class="cost">
-                    <label for="totalCost">Total Cost:</label>
-                    <input type="number" name="totalCost" id="totalCost" min="0" step="0.01">
-                </div>
+                <form action="result.php" name="tippingCalculator" method="post">
+                    <div class="people">
+                        <label for="numberOfPeople">Number of Diners:</label>
+                        <input type="number" name="numberOfPeople" id="numberOfPeople" min="1" placeholder="#" required>
+                    </div>
+                    <div class="cost">
+                        <label for="totalCost">Total Cost ($):</label>
+                        <input type="number" name="totalCost" id="totalCost" min="0" step="0.01" placeholder="$" required>
+                    </div>
 
-                <div class="percent">
-                    <label for="tipPercent">Tip %:</label>
-                    <input type="number" name="tipPercent" id="tipPercent" min="0" step="0.01">
-                </div>
+                    <div class="percent">
+                        <label for="tipPercent">Tip (%):</label>
+                        <input type="number" name="tipPercent" id="tipPercent" min="0" step="0.01" placeholder="%" value="15" required>
+                    </div>
 
-                <input type="submit" value="Calculate">
-
-            </form>
+                    <input type="submit" value="Calculate">
+                </form>
             </div>
-
         </section>
     </main>
 </body>
--- result.php
+++ result.php
@@ -9,9 +9,9 @@
 <body>
     <main>
         <section>
-            <h1>Tipping Calculator</h1>
-            <p>Result:</p>
-            <div class="result">
+                <h1>Tipping Calculator</h1>
+                <p>Result:</p>
+            <div class="result fadeIn">
                 <?php         
                 $numberOfPeople = ($_POST['numberOfPeople']);                
                 $totalCost = ($_POST['totalCost']);
@@ -22,18 +22,19 @@
                 $costPerPerson = $costPlusTip / $numberOfPeople;
                 ?>
 
-                <p>Bill Total: $<?php echo number_format($totalCost,2) ?></p>
-                <p>Tip: $<?php echo number_format($tipAmount,2) . " (" . $tipPercent . "%)"; ?></p>
-                <p>Total Owed: $<?php echo number_format($costPlusTip,2) ?></p>
+                <p>Bill Total: <span>$<?php echo number_format($totalCost,2) ?></span></p>
+                <p>Tip: <span>$<?php echo number_format($tipAmount,2) . " (" . $tipPercent . "%)"; ?></span></p>
+                <p>Total Owed: <span>$<?php echo number_format($costPlusTip,2) ?></span></p>
                 <div class="resultSentence">
-                <?php
-                if($numberOfPeople=='1') {
-                    echo "The single diner owes $" . number_format($costPerPerson,2);
-                } else {
-                    echo "Each of the " . $numberOfPeople . " diners owes $" . number_format($costPerPerson,2);
-                }
-                ?>
+                    <?php
+                    if($numberOfPeople=='1') {
+                        echo "The single diner owes $" . number_format($costPerPerson,2);
+                    } else {
+                        echo "Each of the " . $numberOfPeople . " diners owes $" . number_format($costPerPerson,2);
+                    }
+                    ?>
                 </div>
+                <a class="againLink" href="index.php">Calculate again</a>
             </div>
         </section>
     </main>
